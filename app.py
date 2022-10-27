49085014880ad0dcd247908dcc17b7ccd8510648
lasthuneter956 <jvelandia@utb.edu.co>
2022-10-27 07:56:28 -0500

    actualizacion 2.2

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ def hello_world():#funcion de la pagina inicial
 def get_vehiculo():#funcion de la pagina mostrar los datos de la tabla vehiculo
     try:#el try es para que si hay un error no se caiga el programa
         cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
-        cursor.execute('select Id_vehiculo, Nombre, Modelo, Tipo, Caracteristica, Cantidad, Precio from heroku_978ea61906c2949.vehiculos')
+        cursor.execute('select Id_vehiculo, Nombre, Modelo, Tipo, Caracteristica, Precio from heroku_978ea61906c2949.vehiculos')
         datos = cursor.fetchall()#el fetchall es para que se muestren todos los datos de la consulta
         vehiculos = []#se crea una lista vacia donde se guardaran los datos de los vehiculos
         for fila in datos:#se crea un ciclo for para que se muestren todos los datos de la consulta
-            dato = {'Id_vehiculo': fila[0], 'Nombre': fila[1], 'Modelo': fila[2] , 'Tipo': fila[3], 'Caracteristica': fila[4], 'Cantidad': fila[5] , 'Precio': fila[6]}#se crea un diccionario para que se muestren los datos de los vehiculos
+            dato = {'Id_vehiculo': fila[0], 'Nombre': fila[1], 'Modelo': fila[2] , 'Tipo': fila[3], 'Caracteristica': fila[4], 'Precio': fila[5]}#se crea un diccionario para que se muestren los datos de los vehiculos
             vehiculos.append(dato)#se agrega los datos de los vehiculos a la lista
         return jsonify({'vehiculos': vehiculos, 'message': 'ok'})#se retorna los datos de los vehiculos y un mensaje de ok
     except Exception as e:#el except es para que si hay un error no se caiga el programa
@@ -35,11 +35,11 @@ def get_vehiculo():#funcion de la pagina mostrar los datos de la tabla vehiculo
 def get_vehiculo_id(codigo):#funcion de la pagina leer los vehiculos por el Id_vehiculo
     try:#el try es para que si hay un error no se caiga el programa
         cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
-        sql = "select Id_vehiculo, Nombre, Modelo, Cantidad, Precio, Caracteristica, Tipo from heroku_978ea61906c2949.vehiculos where Id_vehiculo = '{0}' ".format(codigo)
+        sql = "select Id_vehiculo, Nombre, Modelo, Precio, Caracteristica, Tipo from heroku_978ea61906c2949.vehiculos where Id_vehiculo = '{0}' ".format(codigo)
         cursor.execute(sql)#se ejecuta la consulta
         datos = cursor.fetchone()#el fetchone es para que se muestren solo los datos de la consulta
         if datos != None: #en caso de que el codigo no exista
-            vehiculo = {'Id_vehiculo': datos[0], 'Nombre': datos[1], 'Modelo': datos[2], 'Cantidad': datos[3], 'Precio': datos[4], 'Caracteristica': datos[5], 'Tipo': datos[6]}
+            vehiculo = {'Id_vehiculo': datos[0], 'Nombre': datos[1], 'Modelo': datos[2], 'Precio': datos[3], 'Caracteristica': datos[4], 'Tipo': datos[5]}
             return jsonify({'vehiculo': vehiculo, 'message': 'ok'})#se retorna los datos del vehiculo y un mensaje de ok
         else:#en caso de que no se encuentre el vehiculo se retorna un mensaje de error
             return jsonify({'message': 'error1'})#en caso de que haya un error se retorna un mensaje de error
@@ -50,8 +50,8 @@ def get_vehiculo_id(codigo):#funcion de la pagina leer los vehiculos por el Id_v
 def post_vehiculo():#funcion de la pagina registrar un vehiculo
     try:# el try es para que si hay un error no se caiga el programa
         cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
-        sql = """INSERT INTO heroku_978ea61906c2949.vehiculos(Nombre , Modelo , Tipo , Caracteristica, Cantidad, Precio) 
-        VALUES ('{0}','{1}','{2}','{3}',{4},{5})""".format(request.json['Nombre'], request.json['Modelo'], request.json['Tipo'], request.json['Caracteristica'], request.json['Cantidad'], request.json['Precio'])
+        sql = """INSERT INTO heroku_978ea61906c2949.vehiculos(Nombre , Modelo , Tipo , Caracteristica, Precio) 
+        VALUES ('{0}','{1}','{2}','{3}',{4})""".format(request.json['Nombre'], request.json['Modelo'], request.json['Tipo'], request.json['Caracteristica'], request.json['Precio'])
         cursor.execute(sql)#se ejecuta la consulta
         mysql.connection.commit()#guardar los cambios
         return jsonify({'message': 'vehiculo añadido'})#se retorna un mensaje de vehiculo añadido
@@ -63,7 +63,7 @@ def put_vehiculo(codigo):
     try:#el try es para que si hay un error no se caiga el programa
         int(codigo)#se convierte el codigo a entero
         cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
-        sql = """UPDATE heroku_978ea61906c2949.vehiculos SET Nombre = '{0}', Modelo = '{1}', Tipo = '{2}', Caracteristica = '{3}', Cantidad = {4}, Precio = {5} WHERE Id_vehiculo = '{6}' """.format(request.json['Nombre'], request.json['Modelo'], request.json['Tipo'], request.json['Caracteristica'], request.json['Cantidad'], request.json['Precio'], codigo)
+        sql = """UPDATE heroku_978ea61906c2949.vehiculos SET Nombre = '{0}', Modelo = '{1}', Tipo = '{2}', Caracteristica = '{3}', Precio = {4} WHERE Id_vehiculo = '{5}' """.format(request.json['Nombre'], request.json['Modelo'], request.json['Tipo'], request.json['Caracteristica'], request.json['Precio'], codigo)
         cursor.execute(sql)#se ejecuta la consulta
         mysql.connection.commit()#guardar los cambios
         return jsonify({'message': 'vehiculo actualizado'})#se retorna un mensaje de vehiculo actualizado
